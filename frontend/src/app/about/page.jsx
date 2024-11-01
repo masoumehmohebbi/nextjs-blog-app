d13e4064a946c9f1147af37c02abbce16ed55856
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-01 12:37:53 +0330

    ✨ feat:Added Metadata

---
+++ frontend/src/app/about/page.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    absolute: "About", // ignore %s in parent layout.
+  },
+};
+const About = () => {
+  return <div>About</div>;
+};
+
+export default About;
