e0c7b9d62559a6e15018ff060690fc25989bdaa9
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-06 21:25:01 +0330

    ✨ feat:Handling Not Found Page

--- frontend/src/app/blogs/[postSlug]/page.jsx
+++ frontend/src/app/blogs/[postSlug]/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { notFound } from "next/navigation";
 import React from "react";
 
 const SinglePost = async ({ params }) => {
@@ -6,9 +7,11 @@ const SinglePost = async ({ params }) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${params.postSlug}`
   );
-  const {
-    data: { post },
-  } = await res.json();
+  const { data } = await res.json();
+
+  const { post } = data || {};
+  if (!post) notFound();
+  
   return (
     <div className="text-secondary-600 max-w-screen-md mx-auto">
       <h1 className="text-secondary-700 text-2xl font-bold mb-8">
