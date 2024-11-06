c657e9010c930db140027f0181f2b855b9aa79c8
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-06 10:55:01 +0330

    ✨ feat:Streaming with loading.js

--- frontend/src/app/blogs/page.jsx
+++ frontend/src/app/blogs/page.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
-const BlogPage = () => {
-  return <div>Blog list</div>;
+const BlogPage = async () => {
+  await new Promise((res) => setTimeout(res, 3000));
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const {
+    data: { posts },
+  } = await res.json();
+  return (
+    <div>
+      {posts.map((post) => (
+        <div>{post.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default BlogPage;
