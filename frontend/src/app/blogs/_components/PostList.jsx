5fed63e30c0bd3657b25dcd10fa2f2091c8fd023
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-06 13:24:28 +0330

    ✨ feat:Next.js Image Component

--- frontend/src/app/blogs/_components/PostList.jsx
+++ frontend/src/app/blogs/_components/PostList.jsx
@@ -4,13 +4,18 @@ const PostList = async () => {
   const {
     data: { posts },
   } = await res.json();
-  return (
-    <div>
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
       {posts.map((post) => (
-        <div>{post.title}</div>
+        <div
+          key={post._id}
+          className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded-lg"
+        >
+          <CoverImage />
+        </div>
       ))}
     </div>
-  );
+  ) : null;
 };
 
 export default PostList;
