a1698d03b6168a19bf0d4f3d4581ecc226f4bc69
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-06 11:16:09 +0330

    ✨ feat:Streaming With Suspense

--- frontend/src/app/blogs/layout.jsx
+++ frontend/src/app/blogs/layout.jsx
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import CategoryList from "./_components/CategoryList";
+import Spinner from "@/ui/Spinner";
 
 export const metadata = {
   title: "بلاگ ها",
@@ -9,7 +11,9 @@ const Layout = ({ children }) => {
       <h1 className="text-lg font-bold mb-12">لیست بلاگ ها</h1>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 lg:col-span-8 xl:col-span-3 space-y-4 text-secondary-500">
-          <CategoryList />
+          <Suspense fallback={<Spinner />}>
+            <CategoryList />
+          </Suspense>
         </div>
         <div className="col-span-12 lg:col-span-8 xl:col-span-9">
           {children}
