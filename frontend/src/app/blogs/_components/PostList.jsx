0d3f263844f18e17e1444c6058e0a710335f2d41
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-06 14:15:15 +0330

    ✨ feat:Post Content

--- frontend/src/app/blogs/_components/PostList.jsx
+++ frontend/src/app/blogs/_components/PostList.jsx
@@ -13,11 +13,27 @@ const PostList = async () => {
           key={post._id}
           className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-300 p-2 rounded-lg"
         >
-          <CoverImage
-            title={post.title}
-            coverImageUrl={post.coverImageUrl}
-            slug={post.slug}
-          />
+          <CoverImage {...post} />
+          {/* post content */}
+          <div>
+            <Link href={`/blogs/${post.slug}`}>
+              <h2 className="mb-4 font-bold text-secondary-700 hover:text-primary-900 transition-all ease-out">
+                {post.title}
+              </h2>
+            </Link>
+
+            {/* post author - readingTime */}
+            <div className="flex items-center justify-between mb-4">
+              <Author {...post.author} />
+              <div className="flex items-center text-[10px] text-secondary-500">
+                <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
+                <span className="ml-1"> خواندن:</span>
+                <span className="ml-1 leading-3">{post.readingTime}</span>
+                <span>دقیقه</span>
+              </div>
+            </div>
+            {/* <PostInteraction post={post} /> */}
+          </div>
         </div>
       ))}
     </div>
