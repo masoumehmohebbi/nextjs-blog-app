50e0234fc5c88a00e3f64da316cd3a48ff3844fd
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2024-11-29 16:33:35 +0330

    ✨ feat:Generate Static Params in Our App

--- frontend/src/app/blogs/_components/PostList.jsx
+++ frontend/src/app/blogs/_components/PostList.jsx
@@ -3,13 +3,17 @@ import CoverImage from "./CoverImage";
 import Author from "./Author";
 import { ClockIcon } from "@heroicons/react/24/outline";
 import PostInteraction from "./PostInteraction";
+import { getPosts } from "@/services/postServices";
 
 const PostList = async () => {
   await new Promise((res) => setTimeout(res, 3000));
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
-  const {
-    data: { posts },
-  } = await res.json();
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  // const {
+  //   data: { posts },
+  // } = await res.json();
+
+  const posts = await getPosts();
+
   return posts.length > 0 ? (
     <div className="grid grid-cols-12 gap-8">
       {posts.map((post) => (
--- frontend/src/services/postServices.js
+++ frontend/src/services/postServices.js
@@ -1,4 +1,4 @@
-async function getPostBySlug(slug) {
+export async function getPostBySlug(slug) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${slug}`
   );
@@ -6,4 +6,11 @@ async function getPostBySlug(slug) {
 
   const { post } = data || {};
   return post;
+}
+
+export async function getPosts() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const { data } = await res.json();
+  const { posts } = data || {};
+  return posts;
 }
